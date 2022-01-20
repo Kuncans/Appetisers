5bba77629d2db7aec96e629814366604113b9493
Kuncans <akafauna@gmail.com>
2022-01-20 23:04:37 +0000

    Account view UI

--- Appetisers/Screens/AccountView/AccountView.swift
+++ Appetisers/Screens/AccountView/AccountView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var unlimitedRefills = false
+
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("😬 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birth Date", selection: $birthDate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+
+                }
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Unlimited Refills", isOn: $unlimitedRefills)
+                }.toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+
+            }
+            .navigationTitle("😬 Account")
         }.navigationViewStyle(.stack)
-        
     }
 }
 
