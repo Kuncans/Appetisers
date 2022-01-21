2c87efab63fd63e5b15832b07ddd7a82d41aeb61
Kuncans <akafauna@gmail.com>
2022-01-21 19:49:25 +0000

    Removed unnecessary ViewModel on OrderView, replaced with @EnvironmentObject order

--- Appetisers/Model/Appetiser.swift
+++ Appetisers/Model/Appetiser.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Appetiser: Decodable, Identifiable, Equatable {
+struct Appetiser: Decodable, Identifiable {
     let id: Int
     let name: String
     let description: String
